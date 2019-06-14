f0408b4530ee7d32d34bddae9220990481aa2afc
Slavik <slavik.kuz.mail@gmail.com>
2019-06-14 13:07:28 +0200

    tests added

---
+++ TestHotel/DBTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelDB;
+
+namespace TestHotel
+{
+    [TestClass]
+    public class DBTest
+    {
+        [TestMethod]
+        public void TestSqlConnection()
+        {
+            SQL sql;
+            sql = new SQL();
+            string result = sql.Scalar("SELECT max(id) FROM Client");
+            Assert.AreEqual(result, "7");
+        }
+    }
+}
